44a4168dcd64e9111af59d786e1fe6c954fe4273
barodaumesh-code <barodaumesh@gmail.com>
2026-04-17 18:46:47 +0530

    Delete shreeshar-gummies/shreeshar-gummies directory

--- shreeshar-gummies/shreeshar-gummies/src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { isAdminAuthenticated } from '@/lib/admin-auth';
-
-export default async function AdminRootPage() {
-  const authed = await isAdminAuthenticated();
-  if (!authed) redirect('/admin/login');
-  redirect('/admin/orders');
-}
